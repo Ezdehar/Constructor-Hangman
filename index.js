c61cfabf34a511a0c10ea78e8dc04888cdc8f6e5
Ezdehar Jaber <ezdeharjaber@Ezdehars-MacBook-Pro.local>
2018-04-13 23:00:35 -0400

    update

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 //this file will contain the logic for the course of the game, which depends on words.js
 //this file will RANDOMLY select a word and uses the WORD CONSTRUCTOR to store it
 //this file will PROMPT the user for each guess and KEEPS TRACK of the users remaining guess
+require('dotenv').config();
+
 var word = require("./word");
