df4f7e8945212d52a8fb37f5e0f6c807b7a933b8
Ezdehar Jaber <ezdeharjaber@Ezdehars-MacBook-Pro.local>
2018-04-13 22:55:27 -0400

    update

--- letter.js
+++ letter.js
@@ -3,35 +3,31 @@
     //the user has guessed the letter. 
 
 console.log("Letters loaded");
-// constructor function for creating letter objects
-var Letter = function(user, guess, character, underscore) {
-    this.user = user;
-    this.character = character;
-    this.underscore = underscore;
-    this.guess = guess;
-  };
-
-  var theLetter = new Letter();
-  console.log("theLetter.constructor is " + theLetter.constructor);
-  
-  // Prints out slightly different information about a letter based on their guess
-  this.readGuess = function() {
-  
-      var guess = "";
-  
-      if (this.character === "") {
-         guess = "";
-      }
-      else {
-        guess = "";
-      }
-      this.printGuess = function() {
-          console.log("User: " + this.user + "\nGuess: " + this.guess +
-          "\nCharacter: " + this.character + "/n_ _ _ _ _ _ _ _");
+// constructor function for creating letter objects that contain properties.
+//the letter objects can take actions but these are referred to as methods
+function Letter(character, underscore, guess) {
+    this.character = true;
+    this.underscore = false;
+    this.Guess = function() {
+        if(this.character === true) {
+            console.log("CORRECT!");
+        }
+        else if(this.underscore === false) {
+            console.log("_");
+        }
+        if (err) {
+            console.log(err);
+          }        
     };
-  }
-  
-  
-  
+};
+// //Prints out slightly different information about a letter based on their guess
+this.printGuess = function() {
+    console.log("\nCharacter: " + this.character + "\nUnderscore: " + this.underscore);
+    console.log("\n_ _ _ _ _ _ _ _ _ _ _");
+
+var answer = new Letter("C", "O", "D", "I", "N", "G", "B", "O", "O", "T", "C", "A", "M", "P");
+answer.printGuess();
+
   // exporting our letter constructor
   module.exports = Letter;
+};
--- word.js
+++ word.js
@@ -2,10 +2,10 @@
 //Using this file to create an OBJECT REPRESENTING the current word the user is attempting to guess
 //This file will be used by index.js to store the word
 
-
+var fs = require("fs");
 var letter = require("./letter");
 
-var fs = require("fs");
+
 
 //An array of new Letter objects representing the letters of the underlying word
 //function that returns a string representing a word
